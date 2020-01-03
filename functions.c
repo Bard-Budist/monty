bc7edca73c4645155de3900b217500ad8260c177
Lina María Montaño Ramírez - Calypso Brontë 🦄 ◬ <codelinamaria@gmail.com>
2020-01-03 07:05:53 +0000

    The opcode pop removes the top element of the stack.

--- functions.c
+++ functions.c
@@ -49,3 +49,24 @@ void _pint(stack_t **stack, unsigned int line_number)
     printf("%d\n", (*stack)->n);
 }
 
+void _pop(stack_t **stack, unsigned int line_number)
+{
+    stack_t *tmp = *stack;
+
+    if (*stack == NULL)
+    {
+        printf("L%d: can't pop an empty stack\n", line_number);
+        exit(EXIT_FAILURE);
+    }
+    if ((*stack)->next != NULL)
+    {
+        *stack = (*stack)->next;
+        (*stack)->prev = NULL;
+    }
+    else
+    {
+        free(tmp);
+        *stack = NULL;
+    }
+}
+
