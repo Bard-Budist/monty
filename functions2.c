720f06199a96928ff0ab64c85f7445f401c9bcf7
Lina María Montaño Ramírez - Calypso Brontë 🦄 ◬ <codelinamaria@gmail.com>
2020-01-05 05:54:30 +0000

    Fix task dic

--- functions2.c
+++ functions2.c
@@ -35,26 +35,25 @@ void _sub(stack_t **stack, unsigned int line_number)
 void _div(stack_t **stack, unsigned int line_number)
 {
 	stack_t *tmp;
-	int cont = 0;
 
-	line_number = line_number;
-	if ((*stack)->n == 0)
+	if ((*stack && (*stack)->next) != '\0')
 	{
-		fprintf(stderr, "L%d: division by zero\n", line_number);
+		fprintf(stderr, "L%d: can't div, stack too short\n", line_number);
+		free_dlistint(stack);
 		exit(EXIT_FAILURE);
 	}
-	if ((*stack)->next != NULL)
+	else if ((*stack)->n == 0)
 	{
-		cont = (*stack)->next->n / (*stack)->n;
-		tmp = (*stack)->next;
-		tmp->n = cont;
-		free(*stack);
-		tmp->prev = NULL;
-		*stack = tmp;
+		fprintf(stderr, "L%d: division by zero\n", line_number);
+		free_dlistint(stack);
+		exit(EXIT_FAILURE);
 	}
 	else
 	{
-		fprintf(stderr, "L%d: can't div, stack too short\n", line_number);
-		exit(EXIT_FAILURE);
+		(*stack)->next->n /= (*stack)->n;
+		tmp = *stack;
+		*stack = (*stack)->next;
+		(*stack)->prev = NULL;
+		free(tmp);
 	}
 }
