c7986c43dd534815f67b3a3521681c484794118a
GiovanniAyala <op7puro@live.com>
2020-01-05 03:56:13 +0000

    Change conditional

--- tools.c
+++ tools.c
@@ -74,7 +74,7 @@ void _nop(stack_t **stack, unsigned int line_number)
 */
 void _pint(stack_t **stack, unsigned int line_number)
 {
-	if (*stack == NULL || stack == NULL)
+	if (stack == NULL || *stack == NULL)
 	{
 		fprintf(stderr, "L%u: can't pint, stack empty\n", line_number);
 		exit(EXIT_FAILURE);
